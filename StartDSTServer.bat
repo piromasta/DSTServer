39fd346b12ba5b78ab0d7b89c8dccd5b7dd40695
piromasta <alexong@videotron.ca>
2023-10-23 21:05:12 -0400

    Uncomment important start lines...

--- StartDSTServer.bat
+++ StartDSTServer.bat
@@ -12,6 +12,6 @@ xcopy /DY .\mods\dedicated_server_mods_setup.lua "%SteamLibraryFolder%steamapps\
 
 :: Start Server
 :: c:\steamcmd\steamcmd.exe +login anonymous +app_update 343050 validate +quit
-::cd /D "%SteamLibraryFolder%steamapps\common\%DSTInstallFolder%\bin64"
-::start "Master" dontstarve_dedicated_server_nullrenderer_x64 -console -cluster %ClusterName% -shard Master
-::start "Cave" dontstarve_dedicated_server_nullrenderer_x64 -console -cluster %ClusterName% -shard Caves 
+cd /D "%SteamLibraryFolder%steamapps\common\%DSTInstallFolder%\bin64"
+start "Master" dontstarve_dedicated_server_nullrenderer_x64 -console -cluster %ClusterName% -shard Master
+start "Cave" dontstarve_dedicated_server_nullrenderer_x64 -console -cluster %ClusterName% -shard Caves 
