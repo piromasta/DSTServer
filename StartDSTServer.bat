fae4b12d6355f9341e9037ca797c10b4cd809e18
piromasta <alexong@videotron.ca>
2023-10-23 21:01:48 -0400

    Update start bat to only required dedicated server install location.

--- StartDSTServer.bat
+++ StartDSTServer.bat
@@ -1,4 +1,17 @@
-rem c:\steamcmd\steamcmd.exe +login anonymous +app_update 343050 validate +quit
-cd /D "G:\SteamLibrary\steamapps\common\Don't Starve Together Dedicated Server\bin64"
-start "Master" dontstarve_dedicated_server_nullrenderer_x64 -console -cluster MyDediServer -shard Master
-start "Cave" dontstarve_dedicated_server_nullrenderer_x64 -console -cluster MyDediServer -shard Caves 
+@echo off
+SETLOCAL EnableDelayedExpansion
+
+:: Setup
+set SteamLibraryFolder=G:\SteamLibrary\
+set DSTInstallFolder=Don't Starve Together Dedicated Server
+
+for %%a in (.) do set "ClusterName=%%~nxa"
+::echo %ClusterName%
+
+xcopy /DY .\mods\dedicated_server_mods_setup.lua "%SteamLibraryFolder%steamapps\common\%DSTInstallFolder%\mods"
+
+:: Start Server
+:: c:\steamcmd\steamcmd.exe +login anonymous +app_update 343050 validate +quit
+::cd /D "%SteamLibraryFolder%steamapps\common\%DSTInstallFolder%\bin64"
+::start "Master" dontstarve_dedicated_server_nullrenderer_x64 -console -cluster %ClusterName% -shard Master
+::start "Cave" dontstarve_dedicated_server_nullrenderer_x64 -console -cluster %ClusterName% -shard Caves 
